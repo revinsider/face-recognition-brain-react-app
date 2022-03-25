684bd04f75303377545bf718cb4a1aa833229699
revinsider <dev@makeover.com.pl>
2022-03-25 11:47:30 +0100

    first commit edited

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt4 flex absolute top-0 ">
+      <div className="br2 shadow-2 ">Logo 🧐</div>
+    </div>
+  );
+};
+
+export default Logo;
